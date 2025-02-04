bd1e977fbd7ae248a418083a635c059a5a661d6d
dulinigunasekara <dulini@remotebears.com>
2025-02-04 23:12:49 +0530

    Implement algorithm

--- src/algorithms/searching/binary_search/IterativeBinarySearch.java
+++ src/algorithms/searching/binary_search/IterativeBinarySearch.java
@@ -1,9 +1,26 @@
 package algorithms.searching.binary_search;
 
-public class IterativeBinarySearch implements IBinarySearch{
+public class IterativeBinarySearch implements IBinarySearch {
 
     @Override
     public int search(int[] array, int value) {
-        return 0;
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == value) {
+                return mid;
+            }
+
+            if (array[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
